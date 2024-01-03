eb4527a63f494a1223b426e326a98554717402fc
Raul Valdez Ortega <raulvaor61@gmail.com>
2024-01-03 01:55:19 -0600

    Merge branch 'main' of https://github.com/JOAQUINGALAVIZ/Cartelera-de-cine

---
+++ ORDINARIO/js/events.js
@@ -0,0 +1,5 @@
+// Asignar las funciones a los eventos de clic de los botones respectivos
+document.getElementById('popularesBtn').addEventListener('click', obtenerPeliculasPopulares);
+document.getElementById('vistasActualmenteBtn').addEventListener('click', obtenerPeliculasVistasActualmente);
+document.getElementById('mejoresCalificadasBtn').addEventListener('click', obtenerPeliculasMejoresCalificadas);
+document.getElementById('proximosLanzamientosBtn').addEventListener('click', obtenerProximosLanzamientos);
--- ORDINARIO/js/funciones.js
+++ ORDINARIO/js/funciones.js
@@ -10,3 +10,28 @@ function agregarEventosMouse(pelicula) {
         pelicula.style.transform = "scale(1)";
     });
 }
+
+// Función para mostrar películas
+function mostrarPeliculas(peliculas) {
+    const cineparkDiv = document.getElementById("cinepark");
+    cineparkDiv.innerHTML = '';
+  
+    for (let pelicula of peliculas) {
+      const carteleraDiv = document.createElement("div");
+      carteleraDiv.className = "pelicula";
+  
+      const imgPelicula = document.createElement("img");
+      imgPelicula.src = `https://image.tmdb.org/t/p/w500/${pelicula.poster_path}`;
+      imgPelicula.alt = pelicula.title;
+  
+      const tituloPelicula = document.createElement("h2");
+      tituloPelicula.textContent = pelicula.title;
+  
+      carteleraDiv.appendChild(imgPelicula);
+      carteleraDiv.appendChild(tituloPelicula);
+  
+      cineparkDiv.appendChild(carteleraDiv);
+      agregarEventosMouse(carteleraDiv);
+    }
+  }
+  
