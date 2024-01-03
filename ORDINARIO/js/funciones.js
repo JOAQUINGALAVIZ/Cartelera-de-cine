f77053c97d8dacae0a3d5c10bc73735af96723b2
JOAQUINGALAVIZ <joaquingalaviz04@hotmail.com>
2024-01-03 03:07:24 -0600

    Se agrego la funciones de mostrar genero y duracion de las peliculas

--- ORDINARIO/js/funciones.js
+++ ORDINARIO/js/funciones.js
@@ -11,27 +11,39 @@ function agregarEventosMouse(pelicula) {
     });
 }
 
-// Función para mostrar películas
 function mostrarPeliculas(peliculas) {
     const cineparkDiv = document.getElementById("cinepark");
     cineparkDiv.innerHTML = '';
   
     for (let pelicula of peliculas) {
-      const carteleraDiv = document.createElement("div");
-      carteleraDiv.className = "pelicula";
+      fetch(`https://api.themoviedb.org/3/movie/${pelicula.id}?api_key=${apiKey}`)
+        .then(response => response.json())
+        .then(data => {
+          const carteleraDiv = document.createElement("div");
+          carteleraDiv.className = "pelicula";
   
-      const imgPelicula = document.createElement("img");
-      imgPelicula.src = `https://image.tmdb.org/t/p/w500/${pelicula.poster_path}`;
-      imgPelicula.alt = pelicula.title;
+          const imgPelicula = document.createElement("img");
+          imgPelicula.src = `https://image.tmdb.org/t/p/w500/${pelicula.poster_path}`;
+          imgPelicula.alt = pelicula.title;
   
-      const tituloPelicula = document.createElement("h2");
-      tituloPelicula.textContent = pelicula.title;
+          const tituloPelicula = document.createElement("h2");
+          tituloPelicula.textContent = pelicula.title;
   
-      carteleraDiv.appendChild(imgPelicula);
-      carteleraDiv.appendChild(tituloPelicula);
+          const generoPelicula = document.createElement("p");
+          generoPelicula.textContent = `Género: ${data.genres.map(genre => genre.name).join(', ')}`;
   
-      cineparkDiv.appendChild(carteleraDiv);
-      agregarEventosMouse(carteleraDiv);
+          const duracionPelicula = document.createElement("p");
+          duracionPelicula.textContent = `Duración: ${data.runtime} minutos`;
+  
+          carteleraDiv.appendChild(imgPelicula);
+          carteleraDiv.appendChild(tituloPelicula);
+          carteleraDiv.appendChild(generoPelicula);
+          carteleraDiv.appendChild(duracionPelicula);
+  
+          cineparkDiv.appendChild(carteleraDiv);
+          agregarEventosMouse(carteleraDiv);
+        })
+        .catch(error => console.error('Error:', error));
     }
   }
   
